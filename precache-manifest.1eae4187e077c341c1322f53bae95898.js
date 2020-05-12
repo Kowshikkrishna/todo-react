96a8601060a29c9e4779e73875ee1601ba110011
Kowshikkrishna <kowshikkrishna4@gmail.com>
2020-05-12 17:31:47 +0530

    Updates

---
+++ precache-manifest.1eae4187e077c341c1322f53bae95898.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "194d4d1fd89114b81e51a0f8d9d824fe",
+    "url": "/todo-react/index.html"
+  },
+  {
+    "revision": "dcdbcc520b55c509e128",
+    "url": "/todo-react/static/css/main.ae5ce5fc.chunk.css"
+  },
+  {
+    "revision": "811dd5812ab79337f235",
+    "url": "/todo-react/static/js/2.2a2b818d.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/todo-react/static/js/2.2a2b818d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "dcdbcc520b55c509e128",
+    "url": "/todo-react/static/js/main.daf33714.chunk.js"
+  },
+  {
+    "revision": "4fae2030ece927f9f52c",
+    "url": "/todo-react/static/js/runtime-main.4dfb4826.js"
+  }
+]);
